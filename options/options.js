3112ede9ddd033e8f00a1442a5cc187176d61449
jorgen.dahlberg@firn.se <jorgen.dahlberg@firn.se>
2022-12-19 07:31:02 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "booktsts_37935",
   url: "https://booktsts_37935.botics.co",
   api: "https://booktsts_37935.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
